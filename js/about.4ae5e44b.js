2abf1ae714de97548fa23d47001981715a255905
ibarrola <67033451+ibarrola@users.noreply.github.com>
2020-07-05 00:35:56 +0800

    Add files via upload

---
+++ js/about.4ae5e44b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("Event Web Application - About...")])])}],c=n("2877"),l={},s=Object(c["a"])(l,a,u,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=about.4ae5e44b.js.map
